00f80d80641617e05a85816d9c42f24496ad5370
marcelosmbr <development.smbr@gmail.com>
2024-08-19 02:40:30 -0300

    tags removed from post

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -63,16 +63,6 @@ export async function PostList() {
                             <div className="h-20 text-gray-800 break-words text-justify mt-2">
                                 {post.description}
                             </div>
-                            <div className="flex flex-wrap gap-1 mt-2">
-                                {post.tags.split(",").map(item => item.trim()).map((tag: string) => (
-                                    <div
-                                        key={tag}
-                                        className="min-w-fit text-white bg-neutral-800 text-sm font-medium px-2.5 py-0.5 rounded border border-gray-700 inline-flex items-center justify-center"
-                                    >
-                                        {tag}
-                                    </div>
-                                ))}
-                            </div>
                         </div>
                     </Link>
                 )}
