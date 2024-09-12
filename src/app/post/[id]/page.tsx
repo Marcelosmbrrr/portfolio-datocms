c1be7c0222069c7474fc80c52b051641738a5eae
marcelosmbr <development.smbr@gmail.com>
2024-09-12 18:21:52 -0300

    post att

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,6 +2,63 @@ import * as React from "react";
 import { performRequest } from "@/libs/datocms";
 import { format } from "date-fns";
 
+interface Post {
+  id: string;
+  name: string;
+  tags: string;
+  text: {
+    value: {
+      schema: string;
+      document: {
+        type: string;
+        children: Array<Paragraph | List>;
+      };
+    };
+  };
+  image: {
+    url: string;
+  };
+  description: string;
+  category: string;
+  _updatedAt: string;
+}
+
+interface Paragraph {
+  type: "paragraph";
+  children: Array<Span | Link>;
+}
+
+interface List {
+  type: "list";
+  style: "bulleted";
+  children: Array<ListItem>;
+}
+
+interface ListItem {
+  type: "listItem";
+  children: Array<Paragraph>;
+}
+
+interface Span {
+  type: "span";
+  value: string;
+  marks?: Array<string>;
+}
+
+interface Link {
+  type: "link";
+  url: string;
+  children: Array<Span>;
+}
+
+interface Data {
+  post: Post;
+}
+
+interface Response {
+  data: Data;
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const QUERY = `
     query GetPost($id: ItemId) {
@@ -71,30 +128,52 @@ export default async function Page({ params }: { params: { id: string } }) {
               />
               <figcaption>{""}</figcaption>
             </figure>
-            {data.post.text.value.document.children.map(
-              (
-                paragraph: {
-                  type: "paragraph";
-                  children: {
-                    type: "span";
-                    value: string;
-                    marks?: string[];
-                  }[];
-                },
-                index: number
-              ) => (
-                <p key={index} className="my-2">
-                  {paragraph.children.map((span, spanIndex) => (
-                    <span
-                      key={spanIndex}
-                      className={span.marks && span.marks.includes("strong") ? "font-bold text-xl": "text-gray-300"}
-                    >
-                      {span.value}
-                    </span>
-                  ))}
-                </p>
-              )
-            )}
+            {data.post.text.value.document.children.map((block: { type: string; children: any[]; }, index: React.Key | null | undefined) => {
+              if (block.type === "paragraph") {
+                return (
+                  <p key={index} className="my-2">
+                    {block.children.map((span, spanIndex) => (
+                      <span
+                        key={spanIndex}
+                        className={
+                          span.marks && span.marks.includes("strong")
+                            ? "font-bold text-xl"
+                            : "text-gray-300"
+                        }
+                      >
+                        {span.value}
+                      </span>
+                    ))}
+                  </p>
+                );
+              } else if (block.type === "list") {
+                return (
+                  <ul key={index} className="list-disc pl-5">
+                    {block.children.map((listItem, listItemIndex) => (
+                      <li key={listItemIndex}>
+                        {listItem.children.map((paragraph: { children: any[]; }, paragraphIndex: React.Key | null | undefined) => (
+                          <p key={paragraphIndex} className="my-2">
+                            {paragraph.children.map((span, spanIndex) => (
+                              <span
+                                key={spanIndex}
+                                className={
+                                  span.marks && span.marks.includes("strong")
+                                    ? "font-bold text-xl"
+                                    : "text-gray-300"
+                                }
+                              >
+                                {span.value}
+                              </span>
+                            ))}
+                          </p>
+                        ))}
+                      </li>
+                    ))}
+                  </ul>
+                );
+              }
+              return null;
+            })}
           </article>
         </div>
       </div>
